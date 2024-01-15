9ee8c5ac4c03227159dd9eef4cb02cbfcb2856a4
yoonyjin <2yjin1213@gmail.com>
2024-01-15 23:59:33 +0900

    15

---
+++ Programmers/Lv1/예산.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> d, int budget) {
+    int answer = 0;
+    return answer;
+}
