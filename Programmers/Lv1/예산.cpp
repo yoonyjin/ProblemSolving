c71aea068ff4e6f4a915970222938f2943d4ff40
yoonyjin <2yjin1213@gmail.com>
2024-01-16 23:27:43 +0900

    240116

--- Programmers/Lv1/예산.cpp
+++ Programmers/Lv1/예산.cpp
@@ -1,11 +1,16 @@
-#include <iostream>
-#include <stdio.h>
-#include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int solution(vector<int> d, int budget) {
     int answer = 0;
+    sort(d.begin(), d.end());
+    for (int i = 0; i < d.size(); i++)
+    {
+        if (0 <= (budget -= d[i])) {
+            answer++;
+        }
+    }
     return answer;
 }
