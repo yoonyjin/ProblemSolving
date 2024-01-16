e8b55d5102dfc6fdfbbded5a7d827f58b2099517
yoonyjin <2yjin1213@gmail.com>
2024-01-16 23:25:05 +0900

    240116

--- Programmers/Lv1/3진법_뒤집기.cpp
+++ Programmers/Lv1/3진법_뒤집기.cpp
@@ -1,10 +1,23 @@
 #include <string>
 #include <vector>
+#include <cmath>
 
 using namespace std;
 
 int solution(int n) {
-    int answer = 0;   
-	
-    return answer;
+    int mod, ten = 0;
+    vector<int> three;
+
+    while (0 < n)
+    {
+        mod = n % 3;
+        n /= 3;
+        three.push_back(mod);
+    }
+
+    for (int i = 0; i < three.size(); i++)
+    {
+        ten += (pow(3, three.size() - i - 1) * (three[i]));
+    }
+    return ten;
 }
