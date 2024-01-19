fd11521ac25d6e88202411a784a8fd2b85171380
yoonyjin <2yjin1213@gmail.com>
2024-01-20 00:47:14 +0900

    240120

--- Programmers/Lv1/이상한_문자_만들기.cpp
+++ Programmers/Lv1/이상한_문자_만들기.cpp
@@ -1,12 +1,22 @@
 #include <string>
 #include <vector>
 
-#include <cctype>
-#include <sstream>
-
 using namespace std;
 
 string solution(string s) {
-    string answer = "";
-    return answer;
+    int idx = 0;
+
+    for (int i = 0; i < s.length(); i++)
+    {
+        // 공백 문자가 나오면 각 단어의 인덱스 초기화
+        if (' ' == s[i])
+        {
+            idx = 0;
+            continue;
+        }
+        // 인덱스가 0,짝수 : 대문자 , 홀수 : 소문자 변환
+        s[i] = (0 == idx % 2) ? toupper(s[i]) : tolower(s[i]);
+        idx++;
+    }
+    return s;
 }
