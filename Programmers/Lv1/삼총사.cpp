c44d6e78c99a850e893e104b04be61fd110de544
yoonyjin <2yjin1213@gmail.com>
2024-01-29 22:55:29 +0900

    240129

--- Programmers/Lv1/삼총사.cpp
+++ Programmers/Lv1/삼총사.cpp
@@ -4,17 +4,17 @@
 using namespace std;
 
 int solution(vector<int> number) {
-    int answer = 0;
-
-    for (int i = 0; i < number.size() - 2; i++) {
-        for (int j = i + 1; j < number.size() - 1; j++) {
-            for (int k = j + 1; k < number.size(); k++) {
-                if ((number[i] + number[j] + number[k]) == 0) {
-                    answer++;
-                }
-            }
-        }
-    }
-
-    return answer;
+	int answer = 0;
+	for (int i = 0; i < number.size(); i++)
+	{
+		for (int j = i + 1; j < number.size(); j++)
+		{
+			for (int z = j + 1; z < number.size(); z++)
+			{
+				if (0 == number[i] + number[j] + number[z])
+					answer++;
+			}
+		}
+	}
+	return answer;
 }
--- Programmers/Lv1/크기가_작은_부분문자열.cpp
+++ Programmers/Lv1/크기가_작은_부분문자열.cpp
@@ -5,5 +5,12 @@ using namespace std;
 
 int solution(string t, string p) {
     int answer = 0;
+    string sub;
+    for (int i = 0; i < t.size() - p.size() + 1; i++)
+    {
+        sub = t.substr(i, p.size());
+        if (p >= sub)
+            answer++;
+    }
     return answer;
 }
