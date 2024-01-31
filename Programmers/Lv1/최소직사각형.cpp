b7876acfde5945f4231e8d105d00e35e54cd650f
yoonyjin <2yjin1213@gmail.com>
2024-02-01 00:07:57 +0900

    240201수정

--- Programmers/Lv1/최소직사각형.cpp
+++ Programmers/Lv1/최소직사각형.cpp
@@ -6,22 +6,20 @@
 using namespace std;
 
 int solution(vector<vector<int>> sizes) {
-    int answer = 0;
-    int width = 0, length = 0;
+    int width = 0, height = 0;
     for (int i = 0; i < sizes.size(); i++)
     {
-        if (sizes[i][0] < sizes[i][1])
-            swap(sizes[i][0], sizes[i][1]);
         // 처음 풀이
-        if (width < sizes[i][0])
-            width = sizes[i][0];
-        if (length < sizes[i][1])
-            length = sizes[i][1];
-    }
-    answer = width * length;
-    return answer;
-}
+        //if (sizes[i][0] < sizes[i][1])
+        //    swap(sizes[i][0], sizes[i][1]);
+        //if (width < sizes[i][0])
+        //    width = sizes[i][0];
+        //if (height < sizes[i][1])
+        //    height = sizes[i][1];
 
-void main() {
-    solution({ {60, 50}, {30, 70}, {60, 30}, {80, 40} });
+        // max() 활용
+        width = max(width, min(sizes[i][0], sizes[i][1]));
+        height = max(height, max(sizes[i][0], sizes[i][1]));
+    }
+    return width * height;
 }
