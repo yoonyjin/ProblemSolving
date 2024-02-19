a8f6bc2c25c755450f56a5b9214c3cc04267ed39
yoonyjin <2yjin1213@gmail.com>
2024-02-19 23:13:10 +0900

    240219

---
+++ Programmers/Lv1/하노이_탑.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+void hanoi(int n, int begin, int mid, int end) {
+	if (1 == n) {
+		cout << begin << ' ' << end;
+		return;
+	}
+}
+
+int main() {
+	int N;
+	cin >> N;
+}
