afff4cbf62e7c89b5e106c6d229637082b8390bc
yoonyjin <2yjin1213@gmail.com>
2025-01-20 23:58:48 +0900

    250120 BJ SOLVED 1

---
+++ Baekjoon/CppSolve/24266_알고리즘_수업_알고리즘의_수행_시간_5.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+// 입력 : N
+// 3중 for문 -> n^3 * 값 ..
+
+int main()
+{
+	long long input = 0;
+
+	cin >> input;
+
+	cout << (input * input * input) << "\n" << "3";
+	return 0;
+}
